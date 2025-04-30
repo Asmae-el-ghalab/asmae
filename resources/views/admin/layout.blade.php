b23a066e82dc7fed1625877d3b154758c62c2a68
Sami sdk <samisdk4@gmail.com>
2025-04-30 14:09:14 +0200

    editing from profile photo

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -12,11 +12,11 @@
 <body>
     <div class="menu">
         <ul>
-            <li class="profile" onclick="window.location=''" style="cursor:pointer;">
+            <li class="profile" onclick="window.location='{{ route('profile.edit') }}'"  style="cursor:pointer;">
                 <div class="img-box">
                     <img src="{{ asset('assets/profile.webp') }}" alt="Logo">
                 </div>
-                <h2>souhail</h2>
+                <h2>{{ Auth::user()->name }}</h2>
             </li>
 
 
